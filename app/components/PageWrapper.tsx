6742da3541ec820784685cb850947885d22117f2
Ngwe Sin Sandar <ngwesinsandar112@gmail.com>
2023-06-09 03:37:02 +0630

    added: margin-bottom and fix-responsive

--- app/components/PageWrapper.tsx
+++ app/components/PageWrapper.tsx
@@ -3,28 +3,26 @@
 import { motion, AnimatePresence } from "framer-motion";
 import { usePathname } from 'next/navigation';
 
-const PageWrapper = ({children}: {children: React.ReactNode}) => {
+const PageWrapper = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
 
-  return ( 
+  return (
     <>
-    <div>
-      <AnimatePresence>
-      <motion.div
-          key={pathname}
-          initial={{ opacity: 0, }}
-          animate={{ opacity: 1, }}
-          transition={{ delay: 0.75, duration: 0.75, ease: "easeOut" }}
-          exit={{ opacity: 1, }}
-        >
-          <div>
+      <div>
+        <AnimatePresence>
+          <motion.div
+            key={pathname}
+            initial={{ opacity: 0, }}
+            animate={{ opacity: 1, }}
+            transition={{ delay: 0.75, duration: 0.75, ease: "easeOut" }}
+            exit={{ opacity: 1, }}
+          >
             {children}
-          </div>
-        </motion.div>
-      </AnimatePresence>
-    </div>
+          </motion.div>
+        </AnimatePresence>
+      </div>
     </>
-   );
+  );
 }
- 
+
 export default PageWrapper;
