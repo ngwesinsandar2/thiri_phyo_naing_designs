13e05a7f57eaea1f137d09e79535520bb7e4de2f
Ngwe Sin Sandar <ngwesinsandar112@gmail.com>
2023-06-10 20:56:42 +0630

    changed to /metaImg.png

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata = {
   title: 'Thiri Phyo Naing\'s designs',
   description: 'These are practices of Thiri Phyo Naing\'s designs',
   openGraph: {
-    images: '../public/metaImg.png',
+    images: '/metaImg.png',
   },
 }
 
